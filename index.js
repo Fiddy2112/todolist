7cfe6028d69644690e19d6e48cf190b07948c910
Fiddy <thnhlong2112@gmail.com>
2024-08-08 15:12:22 +0700

    create dockerfile

--- index.js
+++ index.js
@@ -12,37 +12,81 @@ app.use(express.json());
 const port = process.env.PORT;
 
 // Initialize provider and signer
-const provider = new ethers.providers.JsonRpcProvider("http://localhost:8545");
+const provider = new ethers.providers.JsonRpcProvider(
+  `${process.env.SEPOLIA_RPC}`
+);
 // const provider = new ethers.providers.Web3Provider(window.ethereum);
 const privateKey = process.env.PRIVATE_KEY; // Ensure you have a private key for the signer
-// const wallet = new ethers.Wallet(privateKey, provider);
-const signer = provider.getSigner();
-const contract = new ethers.Contract(contractAddress, contractABI.abi, signer);
+const wallet = new ethers.Wallet(privateKey, provider);
+// const signer = provider.getSigner();
+// const contract = new ethers.Contract(contractAddress, contractABI.abi, signer);
+const contract = new ethers.Contract(contractAddress, contractABI.abi, wallet);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./pages/index.html"));
 });
 
+app.get("/addTask", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pages/addTask.html"));
+});
+
+app.get("/addTask.html", (req, res) => {
+  res.sendFile(path.join(__dirname, "./pages/addTask.html"));
+});
+
+// app.post("/addTask", async (req, res) => {
+//   var task = req.body.desc;
+
+//   async function getDataBlockchain(task) {
+//     console.log("Adding task in blockchain network...");
+//     const tx = await contract.addTask(task);
+//     console.log(tx);
+//     await tx.wait();
+//   }
+
+//   await getDataBlockchain(task);
+//   res.send("The task has been registered in the smart contract");
+// });
+
 app.post("/addTask", async (req, res) => {
   const task = req.body.desc;
-  console.log(task);
+  console.log("Received task:", task);
+
+  if (typeof task !== "string") {
+    return res
+      .status(400)
+      .send("Invalid task description. It should be a string.");
+  }
+
   try {
-    console.log(contract);
     console.log("Adding task in blockchain network...");
     const tx = await contract.addTask(task);
-    console.log(tx);
     await tx.wait();
-    res.send("The task has been registered in the smart contract");
+    res.status(200).json("The task has been registered in the smart contract");
+    alert("Add task completed");
   } catch (error) {
     console.error("Error adding task:", error);
-    res.status(500).send("Failed to add the task to the smart contract");
+    res.status(500).json("Failed to add the task to the smart contract");
   }
 });
 
+// app.post("/changeTask", async (req, res) => {
+//   try {
+//     const id = req.body.id;
+//     // Make sure to use the correct method for updating a task
+//     console.log("Changing task in blockchain network...");
+//     const tx = await contract.changeTask(id); // Assuming you have a `changeTask` method in your contract
+//     await tx.wait();
+//     res.send("The task has been changed in the smart contract");
+//   } catch (error) {
+//     console.error("Error changing task:", error);
+//     res.status(500).send("Failed to change the task in the smart contract");
+//   }
+// });
+
 app.post("/changeTask", async (req, res) => {
+  const id = req.body.id;
   try {
-    const id = req.body.id;
-    // Make sure to use the correct method for updating a task
     console.log("Changing task in blockchain network...");
     const tx = await contract.changeTask(id); // Assuming you have a `changeTask` method in your contract
     await tx.wait();
